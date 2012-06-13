64da7411c014911ac16340be7c310070cfc51436
rubenpaixao <rubenpaixao@rubenair-pc.lan>
2012-06-13 02:52:43 +0100

    -DBConnect & SQLInstruct created

---
+++ barulhentos-javaapi/src/db/SQLInstruct.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package db;
+
+import java.io.Serializable;
+
+public class SQLInstruct implements Serializable {
+
+    //Database Configs:
+//    public static final String dbAdress = "jdbc:mysql://localhost:3306/etoilev3";
+//    public static final String dbUsername = "root";
+    //public static final String dbPassword = "";
+//    public static final String dbPassword = "etoile";
+//    public static final String dbAdress = "jdbc:mysql://localhost:3306/etoilev3";
+//    public static final String dbUsername = "root";
+//    public static final String dbPassword = "";
+    //public static final String dbPassword = "etoile";
+//   S public static final String dbAdress = "jdbc:mysql://localhost:3306/etoilepl_etoilev3";
+    public static final String dbAdress = "jdbc:mysql://84.40.22.48:3306/etoilepl_etoilev3";
+    public static final String dbUsername = "etoilepl_etoile";
+    public static final String dbPassword = "WryDiluteQuirkyRider";
+    
+    // fazer no cmd ssh -L 3306:localhost:3306 -l user v3.etoilecascadesideas.eu
+//    Example:
+//    public static String getNews(){
+//       return "SELECT id,title,news,url, professor FROM news"; 
+//    }
+
+  
+    
+
+ 
+}
